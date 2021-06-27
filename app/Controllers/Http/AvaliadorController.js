2375c69d7787c79518d90ed7cfa2ed3fd532972d
Marcos <Vinicius>
2021-06-26 22:39:58 -0400

    adicionando mudanças nos models

--- app/Controllers/Http/AvaliadorController.js
+++ app/Controllers/Http/AvaliadorController.js
@@ -1,81 +1,52 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
-
-/**
- * Resourceful controller for interacting with avaliadors
- */
+const Avaliador = use('App/Models/Avaliador')
 class AvaliadorController {
-  /**
-   * Show a list of all avaliadors.
-   * GET avaliadors
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
+ 
+  async index ({ response }) {
+      const avaliador = await Avaliador.all()
+
+      response.status(200).json({
+        message: 'Seus Registros',
+        data: avaliador
+      })
   }
 
-  /**
-   * Render a form to be used for creating a new avaliador.
-   * GET avaliadors/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
 
-  /**
-   * Create/save a new avaliador.
-   * POST avaliadors
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
+ 
   async store ({ request, response }) {
+    const { nome, email, password, trabalho_id } = request.post()
+
+    const profe = await Avaliador.create({ nome, email, password, trabalho_id })
+
+    response.status(201).json({
+      message: 'Cadastro feito com sucesso!',
+      data: profe
+    })
   }
 
-  /**
-   * Display a single avaliador.
-   * GET avaliadors/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
+  async show ({  request, response, view }) {
+      response.status(200).json({
+        message: 'Here is your task.',
+        data: request.post().profe
+    })
   }
+  
 
-  /**
-   * Render a form to update an existing avaliador.
-   * GET avaliadors/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
+  async update ({ params: {id}, request, response }) {
+    const { nome, email, password, trabalho_id, profe } = request.post()
 
-  /**
-   * Update avaliador details.
-   * PUT or PATCH avaliadors/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
+    profe.nome = nome
+    profe.email = email
+    profe.password = password
+    profe.trabalho_id = trabalho_id
+
+    await profe.save()
+
+    response.status(200).json({
+      message: 'atualizado com sucesso!!',
+      data: profe
+    })
   }
 
   /**
